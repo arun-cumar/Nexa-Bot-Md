771ec56dd4b14d765065acf295d98942472e7f15
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-17 07:44:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-import { startNexa } from './connection.js' 
-import config from "./config.js"
-console.log(config.BOT_NAME)
-console.log(config.OWNER_NUMBER)
-console.log(config.SESSION_ID)
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys";
+import connectionHandler from "./settings/connection.js"; 
+import config from "./config.js";
+
+console.log(`Starting ${config.BOT_NAME}...`);
+
+async function startNexa() {
+    const { state, saveCreds } = await useMultiFileAuthState('./session');
+
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: true,
+        browser: [config.BOT_NAME, "Chrome", "1.0.0"]
+    });
+
+    connectionHandler(sock, startNexa, saveCreds);
+}
+
+startNexa();
