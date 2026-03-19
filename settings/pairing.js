d8ed9c64d8aa82e75f409d98b85fba2b32bc2193
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-19 19:39:21 +0530

    Update pairing.js

--- settings/pairing.js
+++ settings/pairing.js
@@ -1,5 +1,8 @@
+export async function handlePairing(sock) {
      if (!sock.authState.creds.registered) {
         const phoneNumber = await question('\n📞Enter your Phone Number with Country Code (eg: 91xxxx): ');
         const code = await sock.requestPairingCode(phoneNumber.replace(/[^0-9]/g, ''));
         console.log(`\x1b[32m\nYOUR 🗝 PAIRING CODE: \x1b[1m${code}\x1b[0m\n`);
     }
+
+
