e9da07d975f9a95dde92ee4401d2fb7c10c11c22
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-23 16:14:29 +0530

    Delete plugins/choose.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- plugins/choose.js
+++
@@ -1,6 +0,0 @@
-export default async (sock, msg, args, extra) => {
-    const chat = msg.key.remoteJid;
-    if (args.length < 2) return await sock.sendMessage(chat, { text: "❌ Usage: `.choose option1 option2 option3`" }, { quoted: msg });
-    const chosen = args[Math.floor(Math.random() * args.length)];
-    await sock.sendMessage(chat, { text: `🎯 *I CHOOSE*\n\n*${chosen}*` }, { quoted: msg });
-};
