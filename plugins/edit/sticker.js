e930eae2260d731afd86d89cf0d8304a2d10f74a
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-29 08:28:18 +0530

    Update sticker.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- plugins/edit/sticker.js
+++ plugins/edit/sticker.js
@@ -1,5 +1,5 @@
 // © 2026 arun•°Cumar. All Rights Reserved.
-import { imageToSticker, videoToSticker, gifToSticker } from '../../lib/emix.js';
+import { imageToSticker, videoToSticker, gifToSticker } from '../../lib/store/emix.js';
 import { downloadMedia } from '../../lib/store/download/download.js';
 
 export default async (sock, msg, args) => {
